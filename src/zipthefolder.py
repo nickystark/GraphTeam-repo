f2f1c48084978417e69b203db00c1c1ab8172a90
Alberto Colella <162927029+Abertocoea@users.noreply.github.com>
2025-05-28 01:18:08 +0200

    sistemato zipthefolder.py

--- src/zipthefolder.py
+++ src/zipthefolder.py
@@ -13,7 +13,3 @@ def gzip_folder(folder_path, output_file):
         tar.add(folder_path, arcname=os.path.basename(folder_path))
     print(f"Folder '{folder_path}' has been compressed into '{output_file}'")
 
-# Example usage
-folder_path = "./testfolder/solution"            # Path to the folder you want to compress
-output_file = "./testfolder/solution.gz"         # Output .gz file name
-gzip_folder(folder_path, output_file)
