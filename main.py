5d6899900fa32e256920726d80f2984c2d2df076
Alberto Colella <162927029+Abertocoea@users.noreply.github.com>
2025-05-28 00:08:04 +0200

    sitstemato bene il main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import logging
 from tqdm import tqdm
 
 from src.models import GNN 
+from src.Losses import GCODLoss
+from src.Losses import SCELoss
+
+from torch.optim.lr_scheduler import StepLR
 
 # Set the random seed
 set_seed()
@@ -18,7 +22,7 @@ def add_zeros(data):
     data.x = torch.zeros(data.num_nodes, dtype=torch.long)
     return data
 
-def train(data_loader, model, optimizer, criterion, device, save_checkpoints, checkpoint_path, current_epoch):
+def train(data_loader, model, optimizer, criterion, scheduler, device, save_checkpoints, checkpoint_path, current_epoch):
     model.train()
     total_loss = 0
     for data in tqdm(data_loader, desc="Iterating training graphs", unit="batch"):
@@ -29,7 +33,7 @@ def train(data_loader, model, optimizer, criterion, device, save_checkpoints, ch
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-
+    scheduler.step()
     # Save checkpoints if required
     if save_checkpoints:
         checkpoint_file = f"{checkpoint_path}_epoch_{current_epoch + 1}.pth"
@@ -109,17 +113,25 @@ def main(args):
     
 
     if args.gnn == 'gin':
-        model = GNN(gnn_type = 'gin', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
+        model = GNN(gnn_type = 'gin', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False, JK='sum', residual=False, graph_pooling='attention' ).to(device)
     elif args.gnn == 'gin-virtual':
-        model = GNN(gnn_type = 'gin', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
+        model = GNN(gnn_type = 'gin', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True , JK='last', residual=True, graph_pooling='attention').to(device)
     elif args.gnn == 'gcn':
         model = GNN(gnn_type = 'gcn', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = False).to(device)
     elif args.gnn == 'gcn-virtual':
         model = GNN(gnn_type = 'gcn', num_class = 6, num_layer = args.num_layer, emb_dim = args.emb_dim, drop_ratio = args.drop_ratio, virtual_node = True).to(device)
     else:
         raise ValueError('Invalid GNN type')
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    criterion = torch.nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01) #weight_decay=0.0001)
+    scheduler = StepLR(optimizer, step_size=10, gamma=0.5)
+    # criterion = torch.nn.CrossEntropyLoss()
+    if args.baseline_mode == 2:
+        #criterion = NoisyCrossEntropyLoss(args.noise_prob)
+        criterion = GCODLoss(args.gamma)
+    else:
+        criterion = SCELoss()
+
 
     # Identify dataset folder (A, B, C, or D)
     test_dir_name = os.path.basename(os.path.dirname(args.test_path))
@@ -165,7 +177,7 @@ def main(args):
 
         for epoch in range(num_epochs):
             train_loss = train(
-                train_loader, model, optimizer, criterion, device,
+                train_loader, model, optimizer, criterion, scheduler, device,
                 save_checkpoints=(epoch + 1 in checkpoint_intervals),
                 checkpoint_path=os.path.join(checkpoints_folder, f"model_{test_dir_name}"),
                 current_epoch=epoch
@@ -197,13 +209,15 @@ if __name__ == "__main__":
     parser.add_argument("--train_path", type=str, help="Path to the training dataset (optional).")
     parser.add_argument("--test_path", type=str, required=True, help="Path to the test dataset.")
     parser.add_argument("--num_checkpoints", type=int, help="Number of checkpoints to save during training.")
-    parser.add_argument('--device', type=int, default=1, help='which gpu to use if any (default: 0)')
+    parser.add_argument('--device', type=int, default=0, help='which gpu to use if any (default: 0)')
     parser.add_argument('--gnn', type=str, default='gin', help='GNN gin, gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
     parser.add_argument('--drop_ratio', type=float, default=0.5, help='dropout ratio (default: 0.5)')
     parser.add_argument('--num_layer', type=int, default=5, help='number of GNN message passing layers (default: 5)')
     parser.add_argument('--emb_dim', type=int, default=300, help='dimensionality of hidden units in GNNs (default: 300)')
     parser.add_argument('--batch_size', type=int, default=32, help='input batch size for training (default: 32)')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 10)')
+    parser.add_argument('--baseline_mode', type=int, default=1, help='1 per SIMMETRYC loss 2 per GCOD loss (default: 1)')
+    parser.add_argument('--gamma', type=float, default=0.2, help='gamma solo per GCODloss (default: 0.2)')
     
     args = parser.parse_args()
     main(args)
