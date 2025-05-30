0bd863926da25dd288bee62902728d63ee96f747
Alberto Colella <162927029+Abertocoea@users.noreply.github.com>
2025-05-30 16:17:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -73,10 +73,12 @@ def train(data_loader, model, optimizer, criterion, scheduler, device, save_chec
     total = 0
     
 
-    new_q = anneal_q(current_epoch + 1, max_epoch, q_start, 0.1)
-    criterion.update_q(new_q)
-    new_k = schedule_k(current_epoch + 1, max_epoch, k_start, 0.5)
-    criterion.update_k(new_k)
+    #new_q = anneal_q(current_epoch + 1, max_epoch, q_start, 0.1)
+    #criterion.update_q(new_q)
+    #new_k = schedule_k(current_epoch + 1, max_epoch, k_start, 0.5)
+    #criterion.update_k(new_k)
+
+    
     # Aggiorna le maschere dei pesi
     if (current_epoch + 1) >= update and (current_epoch + 1) % update == 0:
         model.eval()
